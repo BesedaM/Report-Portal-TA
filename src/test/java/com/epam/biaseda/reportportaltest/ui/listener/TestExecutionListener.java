89ebf7c9ced8e32da4ea28ef0d565c002de5c02a
maryia.biaseda <maryia.biaseda@workhuman.com>
2023-04-27 22:43:26 +0400

    [basic] screenshot saver added

--- src/test/java/com/epam/biaseda/reportportaltest/ui/listener/TestExecutionListener.java
+++ src/test/java/com/epam/biaseda/reportportaltest/ui/listener/TestExecutionListener.java
@@ -2,12 +2,22 @@ package com.epam.biaseda.reportportaltest.ui.listener;
 
 import com.epam.biaseda.reportportaltest.core.logger.CustomLogger;
 import com.epam.biaseda.reportportaltest.core.logger.CustomLoggerProvider;
-import com.epam.biaseda.reportportaltest.ui.BaseUITest;
+import com.epam.biaseda.reportportaltest.ui.driver.WebDriverHolder;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.TestNGException;
 
-public class TestExecutionListener extends BaseUITest implements ITestListener {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestExecutionListener implements ITestListener {
 
     private static CustomLogger log = CustomLoggerProvider.getLogger();
 
@@ -15,5 +25,22 @@ public class TestExecutionListener extends BaseUITest implements ITestListener {
     public void onTestFailure(ITestResult iTestResult) {
         log.error(String.format("Test '%s' failed!", iTestResult.getMethod().getConstructorOrMethod().getName()));
         log.error(ExceptionUtils.getStackTrace(iTestResult.getThrowable()));
+
+        try {
+            takeScreenshot();
+        } catch (IOException e) {
+            throw new TestNGException("Unable to take screenshot!");
+        }
+    }
+
+    private void takeScreenshot() throws IOException {
+        File screenshot = ((TakesScreenshot) WebDriverHolder.getWebDriver()).getScreenshotAs(OutputType.FILE);
+
+        String timeString = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss").format(LocalDateTime.now());
+        String pathString = Paths.get(".", "/target/screenshot/" + timeString + ".png").toAbsolutePath().toString();
+
+        File newFile = new File(pathString);
+        FileUtils.copyFile(screenshot, newFile);
+        log.error(String.format("Screenshot %s was taken", newFile.getPath()));
     }
 }
