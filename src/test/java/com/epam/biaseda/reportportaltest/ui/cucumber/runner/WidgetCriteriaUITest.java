f7b409dea2a595f49a2e773bdf1eac4b586ccd48
maryia.biaseda <maryia.biaseda@workhuman.com>
2023-05-12 20:06:03 +0400

    [bdd_cucumber] cucumber tests are running in parallel

--- src/test/java/com/epam/biaseda/reportportaltest/ui/cucumber/runner/WidgetCriteriaUITest.java
+++ src/test/java/com/epam/biaseda/reportportaltest/ui/cucumber/runner/WidgetCriteriaUITest.java
@@ -5,15 +5,17 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = {"src/test/java/com/epam/biaseda/reportportaltest/ui/cucumber/feature/WidgetCriteria.feature"},
+        features = {"src/test/resources/cucumber.feature"},
         glue = {"com.epam.biaseda.reportportaltest.ui.cucumber.hook",
                 "com.epam.biaseda.reportportaltest.ui.cucumber.step"},
-        plugin = {"pretty","com.epam.biaseda.reportportaltest.ui.cucumber.runner.listener.CucumberTestListener"}
+        plugin = {"pretty",
+                "com.epam.biaseda.reportportaltest.ui.cucumber.runner.listener.CucumberTestListener",
+                "html:target/report/report.html"}
 )
 public class WidgetCriteriaUITest extends AbstractTestNGCucumberTests {
 
     @Override
-    @DataProvider(parallel = false)
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
