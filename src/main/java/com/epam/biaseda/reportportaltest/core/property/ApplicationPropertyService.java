a4ffabf6461291ea75d0835fd20f3d2fd57d6f8e
maryia.biaseda <maryia.biaseda@workhuman.com>
2023-06-12 23:41:53 +0400

    [ui] updates

--- src/main/java/com/epam/biaseda/reportportaltest/core/property/ApplicationPropertyService.java
+++ src/main/java/com/epam/biaseda/reportportaltest/core/property/ApplicationPropertyService.java
@@ -12,6 +12,8 @@ public class ApplicationPropertyService {
     private static final String SERVER_LOCAL_URL = "server.local.url";
     private static final String SERVER_REMOTE_URL = "server.remote.url";
     private static final String SERVER_LOCAL_SELENOID_URL = "server.local.selenoid.url";
+    private static final String DRIVER_LOCAL_MODE = "local";
+    private static final String DRIVER_REMOTE_MODE = "remote";
 
     static {
         loadProperties();
@@ -43,9 +45,9 @@ public class ApplicationPropertyService {
     }
 
     private static String defineLocalHostUrl(String webDriverMode) {
-        if (webDriverMode.equalsIgnoreCase("local")) {
+        if (webDriverMode.equalsIgnoreCase(DRIVER_LOCAL_MODE)) {
             return properties.getProperty(SERVER_LOCAL_URL);
-        } else if (webDriverMode.equalsIgnoreCase("remote")) {
+        } else if (webDriverMode.equalsIgnoreCase(DRIVER_REMOTE_MODE)) {
             return properties.getProperty(SERVER_LOCAL_SELENOID_URL);
         }
         throw new IllegalStateException(String.format("Unknown webdriver mode %s passed!", webDriverMode));
